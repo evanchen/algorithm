3e0a8e9c93f4ac33f2a94874e010b9226c05ad7f
evanchen <wqchen1130@126.com>
2016-04-18 21:37:11 +0800

    快排优化

--- algorithm/quick_sort.h
+++ algorithm/quick_sort.h
@@ -4,7 +4,6 @@
 int partition(int* arr, int bpos, int epos)
 {
 	int tarPos = bpos;
-	int key = arr[bpos];
 	while (bpos < epos)
 	{
 		if (arr[bpos] > arr[epos]) {
@@ -30,6 +29,31 @@ int partition(int* arr, int bpos, int epos)
 	return tarPos;
 }
 
+//Let k < l. Suppose Partition(A[k..l]) returns m.
+// 1. k <= m < l;
+// 2.Partition yields a permutation of the original list,
+// where A[k..m] contains only numbers <= A[k]
+// and A[m + 1..l] contains only numbers >= A[k].
+int partition2(int* arr, int bpos, int epos)
+{
+	int keyPos = bpos;
+	bpos--;
+	epos++;
+	while (bpos < epos)
+	{
+		bpos++;
+		while (arr[bpos] < arr[keyPos])
+			bpos++;
+		epos--;
+		while (arr[epos] > arr[keyPos])
+			epos--;
+		if (bpos < epos)
+			swap(&arr[bpos], &arr[epos]);
+	}
+	print(arr, 10);
+	return epos;
+}
+
 void do_quick_sort(int* arr, int bpos, int epos)
 {
 	if (bpos >= epos) return;
@@ -38,7 +62,10 @@ void do_quick_sort(int* arr, int bpos, int epos)
 	do_quick_sort(arr, m + 1,epos);
 }
 
-//快速排序,分治法,相比合并排序，排序时不需要缓存排序结果
+//快速排序,分治法,相比合并排序，排序时可以直接在原数组里排序
+// 平均 O(log2n)  * O(n) = O(n*log2n)
+// 最坏的情况下,arr本身有序 O(n2)
+// partition 方法入手可以减少平均耗时
 void quick_sort(int* arr, int size)
 {
 	do_quick_sort(arr, 0, size - 1);
