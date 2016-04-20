9618366b860ac3e083f90169264c641ce329414c
evanchen <wqchen1130@126.com>
2016-04-20 10:23:46 +0800

    堆排序优化

--- algorithm/binary_tree.h
+++ algorithm/binary_tree.h
@@ -1,7 +1,7 @@
 #pragma once
 #include"util.h"
 
-void heap_order(int* arr, int parent, int size)
+int heap_order(int* arr, int parent, int size)
 {
 	int left = parent * 2;
 	int right = parent * 2 + 1;
@@ -11,8 +11,10 @@ void heap_order(int* arr, int parent, int size)
 		}
 		if (arr[parent - 1] < arr[left - 1]) {
 			swap(&arr[parent - 1], &arr[left - 1]);
+			return left;
 		}
 	}
+	return 0;
 }
 
 void make_heap(int* arr,int parent, int size)
@@ -30,21 +32,26 @@ void rebuild_order(int* arr, int parent, int size)
 	if (parent >= size) return;
 	int left = parent * 2;
 	int right = parent * 2 + 1;
-	heap_order(arr, parent, size);
-	rebuild_order(arr, left, size);
-	rebuild_order(arr, right, size);
+	left = heap_order(arr, parent, size);
+	if(left > 0)
+		rebuild_order(arr, left, size);
 }
 
 //In a binary tree, each node has at most two children.
 //The root carries index 1, and if a node has index i ,
 //then its children (if any) are indexed 2i and 2i + 1.
 //An array of length n gives rise to a binary tree of depth ceil(log2n).
+
+//Finding the largest element in a heap takes O(1).
+//Turning an array of length n into a heap takes O(n).
+//Replacing the element in the root of a heap, and restoring the heap,takes at most O(log n).
 void heap_sort(int* arr, int size)
 {
+	make_heap(arr, 1, size);
+	rebuild_order(arr, 1, size);
 	for (int i = size; i > 0; i--) {
-		make_heap(arr, 1, i);
-		rebuild_order(arr, 1, i);
 		swap(&arr[i-1],&arr[0]);
+		rebuild_order(arr, 1, i-1);
 	}
 	print(arr, 10);
 }
